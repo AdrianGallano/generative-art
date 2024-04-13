f05aa01dfb225b3b78f1fe1c68e4d3a6f38a7d72
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-14 01:22:46 +0800

    Remove STATICFILES_DIRS

--- generative_art/generative_art/settings.py
+++ generative_art/generative_art/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static") 
 STATIC_URL = "/static/" 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 COMPRESS_ROOT = os.path.join(BASE_DIR, "static")
 COMPRESS_ENABLED = True
