4f0252cd4d8882deb18df5f812c7a844aaf66f36
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-14 16:45:32 +0800

    Configure allowed host and database settings

--- generative_art/generative_art/settings.py
+++ generative_art/generative_art/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", "adriangallano.co", "www.adriangallano.co"]
 
 
 # Application definition
@@ -82,17 +82,24 @@ WSGI_APPLICATION = "generative_art.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USERNAME"),
+        "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "ssl": {
+                "ssl_disabled": False,
+            },
+        },
     }
 }
+
 
 
 # Password validation
